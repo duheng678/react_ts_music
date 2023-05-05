99828e8553a968cef091a4cb501f67d2d6383e8a
moondu <duheng678@163.com>
2023-05-05 17:25:32 +0800

    code config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const obj = { name: 'moon' }
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <h2>App</h2>
+      <div>hello react</div>
+    </div>
+  )
 }
 
 App.propTypes = {}
